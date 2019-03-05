babad8c93c5f9182a369f955a8b217460529494a
ItelSunday <iteldomingotech@gmail.com>
2019-03-05 13:47:29 -0800

    updated component structure

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { ToDoForm, ToDoList} from './component'
 
 class App extends Component {
   render() {
